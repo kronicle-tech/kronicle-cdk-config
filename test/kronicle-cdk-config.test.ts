b6fb042d6fcc946aaf34f4e799ae2b3301c39b2f
Simon Dean <simon@simondean.org>
2022-02-07 22:32:30 +0000

    Initial commit

---
+++ test/kronicle-cdk-config.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KronicleCdkConfig from '../lib/kronicle-cdk-config-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/kronicle-cdk-config-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KronicleCdkConfig.KronicleCdkConfigStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
