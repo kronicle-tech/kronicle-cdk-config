4ce5319fa0dbea91111fca2d6030bb5133713529
Simon Dean <simon@simondean.org>
2022-04-02 16:09:46 +0100

    Refactor for clarity (#89)

--- lib/kronicle-stack.ts
+++ lib/kronicle-stack.ts
@@ -13,28 +13,8 @@ export class KronicleStack extends cdk.Stack {
     super(scope, id, props);
 
     const kronicleVersion = "0.1.177";
-
-    const vpc = new ec2.Vpc(this, "KronicleVpc", {
-      vpcName: "kronicle",
-      maxAzs: 2,
-      natGateways: 0,
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: "public",
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-      ],
-    });
-    const cluster = new ecs.Cluster(this, "KronicleEcsCluster", {
-      clusterName: "kronicle",
-      vpc,
-    });
     const domainName = "demo.kronicle.tech";
-    const certificate = new acm.Certificate(this, "KronicleCertificate", {
-      domainName,
-      validation: acm.CertificateValidation.fromDns(),
-    });
+
     // If you want to connect Kronicle to a Git host like GitHub or GitLab, you will probably need to configure Kronicle
     // with access tokens.  These access tokens can be placed in a "secret" in AWS Secrets Manager.  Create a secret
     // in AWS Secrets Manager named "kronicle" with a value like this:
@@ -50,15 +30,112 @@ export class KronicleStack extends cdk.Stack {
       "KronicleServiceConfigSecret",
       "kronicle"
     );
-    const taskDefinition = new ecs.FargateTaskDefinition(
-      this,
-      "KronicleTaskDefinition",
-      {
-        family: "kronicle",
-        cpu: 2_048,
-        memoryLimitMiB: 4_096,
-      }
+
+    let kronicleAppEnvironment = {
+      SERVER_SIDE_SERVICE_BASE_URL: "http://localhost:8090",
+      ANALYTICS_PLAUSIBLE_ENABLED: "true",
+      ANALYTICS_PLAUSIBLE_DATA_DOMAIN: "demo.kronicle.tech",
+      INTRO_TITLE: "Kronicle Live Demo",
+      INTRO_MARKDOWN: `This is a live demo for [Kronicle](https://kronicle.tech). Kronicle is an open source system for
+visualising and documenting a tech stack, including software and infrastructure.
+
+Interesting pages in the demo:
+
+1. [An interactive, dynamically generated dependencies diagram](/all-components/dependencies) that shows all the components in the demo tech stack and how they connect to each other.  Try hovering over items in the diagram and using the filters on the right hand side.
+2. [A list of all the components in the demo tech stack](/all-components).  Try using the filters on the right hand side.
+3. Kronicle fully supports OpenAPI specs.  Kronicle can download OpenAPI specs hosted on a service's endpoint (e.g. with Springdoc) and then [hostes a copy of those OpenAPI specs](/all-components/openapi-specs).  Click a link in the \`Link\` column on that page to view an individual OpenAPI spec rendered using Redoc.
+4. [A list of all the components owned by a team](/teams/kronicle-project/components)
+5. [Dependencies diagram for an individual component](/components/kronicle-service/dependencies).  Use the radius dropdown on the right hand side to see more of the tech stack that surrounds the component.
+6. [A component's page which shows the "key software" used by the component and other important information](/components/kronicle-service).  Kronicle automatically scans Gradle build scripts (npm support coming soon) to find the key software used by the component.
+7. [All the tech debt for the tech stack](/all-components/tech-debts)
+8. [All the cross functional requirements (NFRs) for a team](/teams/kronicle-project/cross-functional-requirements)
+9. [Response times for any components using Zipkin for distributed tracing](/components/kronicle-service/response-times)`,
+    };
+    const kronicleServiceEnvironment = {
+      PLUGINS_GITHUB_ENABLED: "true",
+      PLUGINS_GITHUB_ORGANIZATIONS_0_ACCOUNT_NAME: "kronicle-tech",
+      // PLUGINS_GITHUB_ORGANIZATIONS_1_ACCOUNT_NAME: "kronicle-computers",
+      PLUGINS_AWS_ENABLED: "true",
+      PLUGINS_AWS_PROFILES_0_REGIONS_0: "us-west-2",
+      PLUGINS_SONARQUBE_ENABLED: "true",
+      PLUGINS_SONARQUBE_BASE_URL: "https://sonarcloud.io",
+      PLUGINS_SONARQUBE_ORGANIZATIONS_0: "kronicle-tech",
+      REPO_FINDERS_IGNORED_REPOS_0_URL:
+        "https://github.com/kronicle-tech/kronicle-metadata-repo-template.git",
+      REPO_FINDERS_IGNORED_REPOS_1_URL:
+        "https://github.com/kronicle-tech/kronicle-metadata-codebase-template.git",
+      REPO_FINDERS_IGNORED_REPOS_2_URL:
+        "https://github.com/kronicle-tech/kronicle-argocd-config.git",
+    };
+    const kronicleServiceSecrets = {
+      PLUGINS_GITHUB_ORGANIZATIONS_0_ACCESS_TOKEN_USERNAME:
+        ecs.Secret.fromSecretsManager(
+          kronicleServiceConfigSecret,
+          "kronicle-tech-github-username"
+        ),
+      PLUGINS_GITHUB_ORGANIZATIONS_0_ACCESS_TOKEN_VALUE:
+        ecs.Secret.fromSecretsManager(
+          kronicleServiceConfigSecret,
+          "kronicle-tech-github-access-token"
+        ),
+      // PLUGINS_GITHUB_ORGANIZATIONS_1_ACCESS_TOKEN_USERNAME:
+      //   ecs.Secret.fromSecretsManager(
+      //     kronicleServiceConfigSecret,
+      //     "kronicle-computers-github-username"
+      //   ),
+      // PLUGINS_GITHUB_ORGANIZATIONS_1_ACCESS_TOKEN_VALUE:
+      //   ecs.Secret.fromSecretsManager(
+      //     kronicleServiceConfigSecret,
+      //     "kronicle-computers-github-access-token"
+      //   ),
+    };
+
+    const vpc = this.createVpc();
+    const cluster = this.createEcsCluster(vpc);
+    const certificate = this.createCertificate(domainName);
+    const taskDefinition = this.createFargateTaskDefinition();
+    this.createKronicleAppContainer(
+      taskDefinition,
+      kronicleVersion,
+      kronicleAppEnvironment
     );
+    this.createKronicleServiceContainer(
+      taskDefinition,
+      kronicleVersion,
+      kronicleServiceEnvironment,
+      kronicleServiceSecrets
+    );
+    this.addPolicyStatementsToFargateTaskRole(taskDefinition, [
+      {
+        effect: iam.Effect.ALLOW,
+        actions: ["xray:GetServiceGraph"],
+        resources: ["*"],
+      },
+      {
+        effect: iam.Effect.ALLOW,
+        actions: ["tag:GetResources"],
+        resources: ["*"],
+      },
+      {
+        effect: iam.Effect.ALLOW,
+        actions: ["logs:StartQuery"],
+        resources: ["*"],
+      },
+    ]);
+    this.createApplicationLoadBalancedFargateService(
+      cluster,
+      taskDefinition,
+      certificate
+    );
+  }
+
+  private createKronicleAppContainer(
+    taskDefinition: ecs.FargateTaskDefinition,
+    kronicleVersion: string,
+    environment: {
+      [key: string]: string;
+    }
+  ) {
     taskDefinition.addContainer("KronicleApp", {
       containerName: "kronicle-app",
       image: ecs.ContainerImage.fromRegistry(
@@ -82,27 +159,20 @@ export class KronicleStack extends cdk.Stack {
         retries: 5,
         startPeriod: cdk.Duration.seconds(15),
       },
-      environment: {
-        SERVER_SIDE_SERVICE_BASE_URL: "http://localhost:8090",
-        ANALYTICS_PLAUSIBLE_ENABLED: "true",
-        ANALYTICS_PLAUSIBLE_DATA_DOMAIN: "demo.kronicle.tech",
-        INTRO_TITLE: "Kronicle Live Demo",
-        INTRO_MARKDOWN: `This is a live demo for [Kronicle](https://kronicle.tech). Kronicle is an open source system for
-visualising and documenting a tech stack, including software and infrastructure.
-
-Interesting pages in the demo:
-
-1. [An interactive, dynamically generated dependencies diagram](/all-components/dependencies) that shows all the components in the demo tech stack and how they connect to each other.  Try hovering over items in the diagram and using the filters on the right hand side.
-2. [A list of all the components in the demo tech stack](/all-components).  Try using the filters on the right hand side.
-3. Kronicle fully supports OpenAPI specs.  Kronicle can download OpenAPI specs hosted on a service's endpoint (e.g. with Springdoc) and then [hostes a copy of those OpenAPI specs](/all-components/openapi-specs).  Click a link in the \`Link\` column on that page to view an individual OpenAPI spec rendered using Redoc.
-4. [A list of all the components owned by a team](/teams/kronicle-project/components)
-5. [Dependencies diagram for an individual component](/components/kronicle-service/dependencies).  Use the radius dropdown on the right hand side to see more of the tech stack that surrounds the component.
-6. [A component's page which shows the "key software" used by the component and other important information](/components/kronicle-service).  Kronicle automatically scans Gradle build scripts (npm support coming soon) to find the key software used by the component.
-7. [All the tech debt for the tech stack](/all-components/tech-debts)
-8. [All the cross functional requirements (NFRs) for a team](/teams/kronicle-project/cross-functional-requirements)
-9. [Response times for any components using Zipkin for distributed tracing](/components/kronicle-service/response-times)`,
-      },
+      environment,
     });
+  }
+
+  private createKronicleServiceContainer(
+    taskDefinition: ecs.FargateTaskDefinition,
+    kronicleVersion: string,
+    environment: {
+      [key: string]: string;
+    },
+    secrets: {
+      [key: string]: ecs.Secret;
+    }
+  ) {
     taskDefinition.addContainer("KronicleService", {
       containerName: "kronicle-service",
       image: ecs.ContainerImage.fromRegistry(
@@ -129,66 +199,53 @@ Interesting pages in the demo:
         retries: 5,
         startPeriod: cdk.Duration.seconds(15),
       },
-      environment: {
-        PLUGINS_GITHUB_ENABLED: "true",
-        PLUGINS_GITHUB_ORGANIZATIONS_0_ACCOUNT_NAME: "kronicle-tech",
-        // PLUGINS_GITHUB_ORGANIZATIONS_1_ACCOUNT_NAME: "kronicle-computers",
-        PLUGINS_AWS_ENABLED: "true",
-        PLUGINS_AWS_PROFILES_0_REGIONS_0: "us-west-2",
-        PLUGINS_SONARQUBE_ENABLED: "true",
-        PLUGINS_SONARQUBE_BASE_URL: "https://sonarcloud.io",
-        PLUGINS_SONARQUBE_ORGANIZATIONS_0: "kronicle-tech",
-        REPO_FINDERS_IGNORED_REPOS_0_URL:
-          "https://github.com/kronicle-tech/kronicle-metadata-repo-template.git",
-        REPO_FINDERS_IGNORED_REPOS_1_URL:
-          "https://github.com/kronicle-tech/kronicle-metadata-codebase-template.git",
-        REPO_FINDERS_IGNORED_REPOS_2_URL:
-          "https://github.com/kronicle-tech/kronicle-argocd-config.git",
-      },
-      secrets: {
-        PLUGINS_GITHUB_ORGANIZATIONS_0_ACCESS_TOKEN_USERNAME:
-          ecs.Secret.fromSecretsManager(
-            kronicleServiceConfigSecret,
-            "kronicle-tech-github-username"
-          ),
-        PLUGINS_GITHUB_ORGANIZATIONS_0_ACCESS_TOKEN_VALUE:
-          ecs.Secret.fromSecretsManager(
-            kronicleServiceConfigSecret,
-            "kronicle-tech-github-access-token"
-          ),
-        // PLUGINS_GITHUB_ORGANIZATIONS_1_ACCESS_TOKEN_USERNAME:
-        //   ecs.Secret.fromSecretsManager(
-        //     kronicleServiceConfigSecret,
-        //     "kronicle-computers-github-username"
-        //   ),
-        // PLUGINS_GITHUB_ORGANIZATIONS_1_ACCESS_TOKEN_VALUE:
-        //   ecs.Secret.fromSecretsManager(
-        //     kronicleServiceConfigSecret,
-        //     "kronicle-computers-github-access-token"
-        //   ),
-      },
+      environment,
+      secrets,
     });
-    taskDefinition.addToTaskRolePolicy(
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["xray:GetServiceGraph"],
-        resources: ["*"],
-      })
-    );
-    taskDefinition.addToTaskRolePolicy(
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["tag:GetResources"],
-        resources: ["*"],
-      })
-    );
-    taskDefinition.addToTaskRolePolicy(
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ["logs:StartQuery"],
-        resources: ["*"],
-      })
-    );
+  }
+
+  private createVpc() {
+    return new ec2.Vpc(this, "KronicleVpc", {
+      vpcName: "kronicle",
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+  }
+
+  private createEcsCluster(vpc: ec2.Vpc) {
+    return new ecs.Cluster(this, "KronicleEcsCluster", {
+      clusterName: "kronicle",
+      vpc,
+    });
+  }
+
+  private createCertificate(domainName: string) {
+    return new acm.Certificate(this, "KronicleCertificate", {
+      domainName,
+      validation: acm.CertificateValidation.fromDns(),
+    });
+  }
+
+  private createFargateTaskDefinition() {
+    return new ecs.FargateTaskDefinition(this, "KronicleTaskDefinition", {
+      family: "kronicle",
+      cpu: 2_048,
+      memoryLimitMiB: 4_096,
+    });
+  }
+
+  private createApplicationLoadBalancedFargateService(
+    cluster: ecs.Cluster,
+    taskDefinition: ecs.FargateTaskDefinition,
+    certificate: acm.Certificate
+  ) {
     const service = new ecsPatterns.ApplicationLoadBalancedFargateService(
       this,
       "KronicleEcsService",
@@ -210,5 +267,16 @@ Interesting pages in the demo:
     service.targetGroup.configureHealthCheck({
       path: "/health",
     });
+  }
+
+  private addPolicyStatementsToFargateTaskRole(
+    taskDefinition: ecs.FargateTaskDefinition,
+    policyStatements: ReadonlyArray<iam.PolicyStatementProps>
+  ) {
+    policyStatements.forEach((policyStatement) =>
+      taskDefinition.addToTaskRolePolicy(
+        new iam.PolicyStatement(policyStatement)
+      )
+    );
   }
 }
